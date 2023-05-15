658b605bd6414edc7c97e5b9bd8a6a446f752e21
ThicoProgrammer <102100201+ThicoProgrammer@users.noreply.github.com>
2023-05-15 19:10:10 -0300

    Add files via upload

---
+++ FeedScreen.js
@@ -0,0 +1,54 @@
+import PostCard from "./PostCard"
+import { FlatList } from "react-native-gesture-handler"
+
+let posts = require("./temp_posts.json")
+
+export default class Feed extends Component{
+
+    renderItem = ({ item: post }) => {
+        return <PostCard post={post} />
+    }
+
+    keyExtractor = (item, index) => index.toString()
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <SafeAreaView syle={styles.droidSafeArea} />
+                <View style={styles.appTitle}>
+                    <Text style={styles.appTitleText}>Espectagrama</Text>
+                </View>
+                <View style={styles.cardContainer}>
+                    <FlatList
+                        keyExtractor={this.keyExtractor}
+                        data={posts}
+                        renderItem={this.renderItem}
+                    />
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "darkblue"
+    },
+    droidSafeArea: {
+        marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 35
+    },
+    appTitle: {
+        flex: 0.07,
+        padding: 5,
+        flexDirection: "row",
+        justifyContent: "center"
+    },
+    appTitleText: {
+        color: "white",
+        fontSize: 28
+    },
+    cardContainer: {
+        flex: 0.93
+    }
+})
